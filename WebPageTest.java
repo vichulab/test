9684d8059af7c129baddd77d9dfff64922a262e6
vichulab <plvisalakshi4@gmail.com>
2025-05-20 22:19:11 -0700

    Add files via upload

---
+++ WebPageTest.java
@@ -0,0 +1,32 @@
+package org.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class WebPageTest {
+
+    private static WebDriver driver;
+    @BeforeTest
+    public void openBrowser() throws InterruptedException{
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(10000);
+        driver.get("https://jssateb.ac.in");
+    }
+    @Test
+    public void titleValidationTest(){
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "JSSATE";
+        Assert.assertEquals(actualTitle,expectedTitle);
+        assertTrue(true,"Title should contain JSSATE");
+    }
+    @AfterTest
+    public void closeBrowser() throws InterruptedException{
+        Thread.sleep(10000);
+        driver.quit();
+    }
+}
